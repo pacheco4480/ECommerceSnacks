5faed5ee52d96ff3bd66d7f6c0b10c8c654a5db1
rafaelsantos1000 <rafaasfs@gmail.com>
2024-08-07 19:56:42 +0100

    Increase, decrease and remove shopping cart items

--- ApiECommerce/Controllers/ShoppingCartItemsController.cs
+++ ApiECommerce/Controllers/ShoppingCartItemsController.cs
@@ -3,6 +3,7 @@ using ApiECommerce.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace ApiECommerce.Controllers
 {
@@ -54,6 +55,111 @@ namespace ApiECommerce.Controllers
             catch (Exception) 
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro ao processar a solicitação");
+            }
+        }
+
+        [Authorize]
+        [HttpPut("[action]{productId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> IncreaseQuantity(int productId)
+        {
+            var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+            var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.Email == userEmail);
+
+            if (user == null) 
+            {
+                return NotFound("Utilizador não encontrado.");
+            }
+
+            var shoppingCartItem = await _appDbContext.ShoppingCartItems.FirstOrDefaultAsync(s =>
+            s.ClientId == user.Id && s.ProductId == productId);
+
+            if (shoppingCartItem != null)
+            {
+                shoppingCartItem.Quantity += 1;
+                shoppingCartItem.Total = shoppingCartItem.UnitPrice * shoppingCartItem.Quantity;
+                _appDbContext.Update(shoppingCartItem);
+                await _appDbContext.SaveChangesAsync();
+                return Ok("Foi aumentada a quantidade com sucesso");
+            }
+            else
+            {
+                return NotFound("Nenhum item encontrado no carrinho");
+            }
+        }
+
+
+        [Authorize]
+        [HttpPut("[action]{productId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DecreaseQuantity(int productId)
+        {
+            var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+            var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.Email == userEmail);
+
+            if (user == null)
+            {
+                return NotFound("Utilizador não encontrado.");
+            }
+
+            var shoppingCartItem = await _appDbContext.ShoppingCartItems.FirstOrDefaultAsync(s =>
+            s.ClientId == user.Id && s.ProductId == productId);
+
+            if (shoppingCartItem != null)
+            {
+                if(shoppingCartItem.Quantity > 1)
+                {
+                    shoppingCartItem.Quantity -= 1;
+                }
+                else
+                {
+                    _appDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                    await _appDbContext.SaveChangesAsync();
+                    return Ok("Item removido com sucesso");
+                }
+               
+                shoppingCartItem.Total = shoppingCartItem.UnitPrice * shoppingCartItem.Quantity;
+                _appDbContext.Update(shoppingCartItem);
+                await _appDbContext.SaveChangesAsync();
+                return Ok("Foi diminuida a quantidade com sucesso");
+            }
+            else
+            {
+                return NotFound("Nenhum item encontrado no carrinho");
+            }
+        }
+
+
+        [Authorize]
+        [HttpDelete("{productId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int productId)
+        {
+            var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+            var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.Email == userEmail);
+
+            if (user == null)
+            {
+                return NotFound("Utilizador não encontrado.");
+            }
+
+            var shoppingCartItem = await _appDbContext.ShoppingCartItems.FirstOrDefaultAsync(s =>
+            s.ClientId == user.Id && s.ProductId == productId);
+
+            if (shoppingCartItem != null)
+            {
+                _appDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                await _appDbContext.SaveChangesAsync();
+                return Ok("Item removido com sucesso");
+            }
+            else
+            {
+                return NotFound("Nenhum item encontrado no carrinho");
             }
         }
     }
