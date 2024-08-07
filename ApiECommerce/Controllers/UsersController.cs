392b9865985b5bcfeba0371f3486701fe3a7869b
rafaelsantos1000 <rafaasfs@gmail.com>
2024-08-07 16:18:04 +0100

    Register users

---
+++ ApiECommerce/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApiECommerce.Entities;
+using ApiECommerce.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiECommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UsersController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+
+        [HttpPost("[action]")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Register([FromBody] User user)
+        {
+            var checkUser = await _appDbContext.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+            if (checkUser != null)
+            {
+                return BadRequest("Já existe um utilizador com este email.");
+            }
+
+            _appDbContext.Users.Add(user);
+            await _appDbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
