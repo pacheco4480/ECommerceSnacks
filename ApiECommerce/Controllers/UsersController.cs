f6e87557ab7300554ecf7fadeed4800f2e684a41
rafaelsantos1000 <rafaasfs@gmail.com>
2024-08-07 17:37:57 +0100

    Implement authorize on swagger

--- ApiECommerce/Controllers/UsersController.cs
+++ ApiECommerce/Controllers/UsersController.cs
@@ -2,6 +2,10 @@
 using ApiECommerce.Entities;
 using ApiECommerce.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace ApiECommerce.Controllers
 {
@@ -10,10 +14,12 @@ namespace ApiECommerce.Controllers
     public class UsersController : ControllerBase
     {
         private readonly AppDbContext _appDbContext;
+        private readonly IConfiguration _config;
 
-        public UsersController(AppDbContext appDbContext)
+        public UsersController(AppDbContext appDbContext, IConfiguration config)
         {
             _appDbContext = appDbContext;
+            _config = config;
         }
 
 
@@ -31,6 +37,45 @@ namespace ApiECommerce.Controllers
             _appDbContext.Users.Add(user);
             await _appDbContext.SaveChangesAsync();
             return StatusCode(StatusCodes.Status201Created);
+        }
+
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login([FromBody] User user)
+        {
+            var currentUser = await _appDbContext.Users.FirstOrDefaultAsync(u => u.Email == user.Email && u.Password == user.Password);
+
+            if (currentUser == null)
+            { 
+                return NotFound("O utilizador não existe"); 
+            }
+
+            var key = _config["JWT:Key"] ?? throw new ArgumentNullException("JWT:Key", "JWT:Key cannot be null.");
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, user.Email!)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["JWT:Issuer"],
+                audience: _config["JWT:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(10),
+                signingCredentials: credentials);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new ObjectResult(new
+            {
+                access_token = jwt,
+                token_type = "bearer",
+                user_id = currentUser.Id,
+                user_name = currentUser.Name
+            });
         }
     }
 }
--- ApiECommerce/Program.cs
+++ ApiECommerce/Program.cs
@@ -1,8 +1,60 @@
 using ApiECommerce.Context;
 using ApiECommerce.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// configura a aplicação para autenticar os users usando tokens JWT,
+// verificando o emissor, audiência, tempo de vida e chave de assinatura do emissor
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            //define o emissor e a audiência validas para o token JWT obtidos da aplicação
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            //Define a chave de assinatura usada para assinar e verificar o token JWT.
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]!))
+        };
+    });
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ECommerce Snacks API", Version = "v1" });
+
+    // Define um esquema securo para JWT
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+    // Implementa a autenticação em todos os endpoints da API
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
 
 // Add services to the container.
 
@@ -32,7 +84,7 @@ if (app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
